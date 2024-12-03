dd04081fdd98089ba28aca1e779ebfdc2289ba60
serinrayuni <serinurayuni@graduate.utm.my>
2024-12-03 16:44:15 +0800

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\WithdrawalController;
+use App\Http\Controllers\AccController;
+use App\Http\Controllers\GenController;
 
+use App\Http\Middleware\CheckAccountStatus;
 
 
 Route::get('/', function () {
@@ -20,4 +24,28 @@ Route::post('/kehadiran', [AttendanceController::class, 'markAttendance'])->name
 Route::get('/withdraw', [WithdrawalController::class, 'showWithdrawForm'])->name('withdraw.form');
 Route::post('/withdraw', [WithdrawalController::class, 'processWithdraw'])->name('withdraw.process');
 Route::post('/withdraw/confirm', [WithdrawalController::class, 'confirmWithdrawal'])->name('withdraw.confirm');
+
+Route::get('/home', [GenController::class, 'home'])->name('home');
+
+Route::get('/login', [AccController::class, 'login'])->name("login");
+Route::post('/login', [AccController::class, 'loginPost'])
+    ->name("login.post");
+
+Route::get('/register', [AccController::class, 'register'])->name("register");
+Route::post('/register', [AccController::class, 'registerPost'])
+    ->name("register.post");
+
+Route::get('/resetpassword', [AccController::class, 'resetpassword'])->name("resetpassword");
+Route::post('/resetpassword', [AccController::class, 'resetpasswordPost'])
+    ->name("resetpassword.post");
+
+Route::get('/editAcc', [AccController::class, 'editAcc'])->name("editAcc");
+Route::post('/editAcc', [AccController::class, 'editAccPost'])
+    ->name("editAcc.post");
+
+Route::get('/dashboard', [AccController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+Route::get('/editAcc', [AccController::class, 'editAcc'])->middleware(['auth'])->name('editAcc');
+    
+Route::post('/logout', [AccController::class, 'logout'])->name('logout');
+Route::post('/deleteAcc', [AccController::class, 'deleteAcc'])->name('deleteAcc');
 ?>

6dbd4666bb426b70f2721d5e8a112889f297d004
serinrayuni <serinurayuni@graduate.utm.my>
2024-12-03 15:15:24 +0800

    Add files

---
+++ app/Http/Kernel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    
+    protected $routeMiddleware = [
+        'check.status' => \App\Http\Middleware\CheckStatus::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+    ];
+}
